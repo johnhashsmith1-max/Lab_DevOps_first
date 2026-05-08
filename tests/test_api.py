15c7ab74c6c95afe721ae6fc182d008b1f607bce
johnhashsmith1-max <johnhashsmith1-max@users.noreply.github.com>
2026-05-08 16:21:59 +0300

    Fix import path in tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-from src.predict import app
+from main import app
 
 # Создаем тестовый клиент на основе нашего FastAPI приложения
 client = TestClient(app)
