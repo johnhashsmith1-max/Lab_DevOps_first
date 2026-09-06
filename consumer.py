6b5611a828a780055c1085f3eebcdf6dbd1dec7c
johnhashsmith1-max <johnhashsmith1-max@users.noreply.github.com>
2026-09-06 17:24:01 +0300

    lab4 config

--- consumer.py
+++ consumer.py
@@ -1,42 +1,30 @@
 import os
+# --- БЛОКИРОВКА ПРОКСИ ---
+for key in ['http_proxy', 'https_proxy', 'HTTP_PROXY', 'HTTPS_PROXY']:
+    os.environ.pop(key, None)
+
 import time
 import json
 import hvac
-import socket
 import requests
 from kafka import KafkaConsumer
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-
 # --- 1. Получение секретов из Vault ---
 def get_vault_db_credentials():
-    vault_host = 'vault'
-    vault_port = '8200'
+    time.sleep(3)
+    vault_url = os.getenv('VAULT_ADDR', 'http://10.199.0.20:8200')
     vault_token = os.getenv('VAULT_TOKEN', 'myroot')
-
-    # Принудительно резолвим IPv4 (обход бага IPv6 DNS в связке Debian/requests/Docker)
-    vault_ip = vault_host
-    for attempt in range(5):
-        try:
-            vault_ip = socket.gethostbyname(vault_host)
-            print(f"Consumer: IP Vault успешно определен -> {vault_ip}")
-            break
-        except socket.error as e:
-            print(f"Consumer: Ожидание DNS Docker... ({e})")
-            time.sleep(2)
-
-    vault_url = f"http://{vault_ip}:{vault_port}"
 
     session = requests.Session()
     session.trust_env = False
 
-    client = hvac.Client(url=vault_url, token=vault_token, session=session)
-
-    for attempt in range(5):
+    for attempt in range(10):
         try:
-            print(f"Consumer: Обращение в Vault ({vault_url}), попытка {attempt + 1}/5...")
+            print(f"Consumer: Обращение в Vault ({vault_url}), попытка {attempt + 1}/10...")
+            client = hvac.Client(url=vault_url, token=vault_token, session=session)
             response = client.secrets.kv.v2.read_secret_version(
                 path='db_credentials', raise_on_deleted_version=True
             )
@@ -47,12 +35,11 @@ def get_vault_db_credentials():
             time.sleep(3)
     raise Exception("Не удалось получить секреты из Vault")
 
-
 # --- 2. Настройка БД ---
 credentials = get_vault_db_credentials()
 DB_USER = credentials['username']
 DB_PASS = credentials['password']
-DB_HOST = os.getenv("DB_HOST", "db")
+DB_HOST = os.getenv("DB_HOST", "10.199.0.10")
 DB_PORT = os.getenv("POSTGRES_PORT", "5432")
 DB_NAME = os.getenv("POSTGRES_DB", "mydb")
 
@@ -61,7 +48,6 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-
 class PredictionResult(Base):
     __tablename__ = "predictions"
     id = Column(Integer, primary_key=True, index=True)
@@ -69,12 +55,10 @@ class PredictionResult(Base):
     prediction_label = Column(String)
     probability = Column(Float)
 
-
 Base.metadata.create_all(bind=engine)
 
 # --- 3. Настройка Kafka Consumer ---
-KAFKA_BROKER = os.getenv("KAFKA_BROKER", "kafka:29092")
-
+KAFKA_BROKER = os.getenv("KAFKA_BROKER", "10.199.0.40:29092")
 
 def start_consumer():
     print(f"Consumer: Подключение к Kafka ({KAFKA_BROKER})...")
@@ -105,7 +89,6 @@ def start_consumer():
             print("Consumer: SUCCESS_DB_WRITE")
     finally:
         db.close()
-
 
 if __name__ == "__main__":
     start_consumer()
