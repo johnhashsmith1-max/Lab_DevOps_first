12f6f94bfa4a2db7b60b4260651fb54c68ffe850
johnhashsmith1-max <johnhashsmith1-max@users.noreply.github.com>
2026-09-06 17:48:42 +0300

    fix: remove subnet, add IPv6 socket patch

--- consumer.py
+++ consumer.py
@@ -1,4 +1,5 @@
 import os
+
 # --- БЛОКИРОВКА СИСТЕМНЫХ ПРОКСИ ---
 for key in ['http_proxy', 'https_proxy', 'HTTP_PROXY', 'HTTPS_PROXY']:
     os.environ.pop(key, None)
@@ -7,13 +8,30 @@ import time
 import json
 import requests
 import hvac
+import socket
 from kafka import KafkaConsumer
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# --- ПРИНУДИТЕЛЬНЫЙ IPv4 DNS (ЗАЩИТА ОТ БАГОВ DOCKER/GLIBC) ---
+old_getaddrinfo = socket.getaddrinfo
+
+
+def new_getaddrinfo(host, port, family=0, type=0, proto=0, flags=0):
+    # Жестко подменяем семейство адресов на AF_INET (только IPv4)
+    return old_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
+
+
+socket.getaddrinfo = new_getaddrinfo
+
+
+# -------------------------------------------------------------
+
 def get_vault_db_credentials():
-    time.sleep(3)
+    print("Consumer: Ожидание 6 секунд для инициализации секретов Vault...")
+    time.sleep(6)
+
     vault_url = os.getenv('VAULT_ADDR', 'http://vault:8200')
     vault_token = os.getenv('VAULT_TOKEN', 'myroot')
 
@@ -30,7 +48,7 @@ def get_vault_db_credentials():
             print("Consumer: Секреты успешно получены!")
             return response['data']['data']
         except Exception as e:
-            print(f"Consumer: Ошибка подключения к Vault: {e}")
+            print(f"Consumer: Секреты еще не готовы (ошибка: {e}). Повтор через 3 сек...")
             time.sleep(3)
     raise Exception("Не удалось получить секреты из Vault")
 
@@ -47,6 +65,7 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+
 class PredictionResult(Base):
     __tablename__ = "predictions"
     id = Column(Integer, primary_key=True, index=True)
@@ -54,9 +73,11 @@ class PredictionResult(Base):
     prediction_label = Column(String)
     probability = Column(Float)
 
+
 Base.metadata.create_all(bind=engine)
 
 KAFKA_BROKER = os.getenv("KAFKA_BROKER", "kafka:29092")
+
 
 def start_consumer():
     print(f"Consumer: Подключение к Kafka ({KAFKA_BROKER})...")
